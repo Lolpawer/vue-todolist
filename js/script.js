c7ad85357ed5a25bfad3ff51c123a94434a2ab24
Lolpawer <dadu.petrosino@gmail.com>
2022-01-17 16:35:10 +0100

    todo list skeleton

--- js/script.js
+++ js/script.js
@@ -1,3 +1,27 @@
 new Vue ({
     el: '#app',
+
+    data: {
+        
+        todoList: [
+            {
+                text: 'Comprare il latte',
+                done: true,
+            },
+            {
+                text: 'Fare il bucato',
+                done: true,
+            },
+            {
+                text: "Dimenticare quello che ho fatto il 19 Ottobre del '99",
+                done: false,
+            },      
+            {
+                text: 'Dominare il mondo',
+                done: false,
+            },
+        ],
+
+    },
+
 });
